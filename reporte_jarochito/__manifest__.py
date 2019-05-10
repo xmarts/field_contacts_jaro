f555e101e02f2feeb843b68d6202dfb7ced1933b
Marco <marcocaaguilar@gmail.com>
2019-05-10 09:36:28 -0500

    Creación del manifest

--- reporte_jarochito/__manifest__.py
+++ reporte_jarochito/__manifest__.py
@@ -3,27 +3,27 @@
     'name': "reporte_jarochito",
 
     'summary': """
-        Modificacion de reporte de factura""",
+        Descripción del producto y tipo de tarifa""",
 
     'description': """
-        Long description of module's purpose
+        Creación de campos para la descripción del producto y muestra el tipo de tarifa en la dirección de envío
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Xmarts, Colaborador : Marco Aguilar ",
+    'website': "http://www.xmarts.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sales, Stock, Contacts',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale','product','stock','contacts'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
